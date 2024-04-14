2b57032c16e3b64bf3156e9d39ddd0c0fa6d4e68
TreyPoynter <treypoynter@gmail.com>
2024-04-14 11:47:24 -0500

    Update ApplicationDbContext.cs
    DbSets

--- CardShop/Data/ApplicationDbContext.cs
+++ CardShop/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CardShop.Models.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CardShop.Data
@@ -9,5 +10,12 @@ namespace CardShop.Data
             : base(options)
         {
         }
+
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<CardType> CardTypes { get; set; }
+        public DbSet<Quality> Qualities { get; set; }
+        public DbSet<Rarity> Rarities { get; set; }
+        public DbSet<Sport> Sports { get; set; }
     }
 }
