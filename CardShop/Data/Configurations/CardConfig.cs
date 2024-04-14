ab18e47c10b0e31dbad4c6115e6caa0b97245beb
TreyPoynter <treypoynter@gmail.com>
2024-04-14 11:47:28 -0500

    Create CardConfig.cs

---
+++ CardShop/Data/Configurations/CardConfig.cs
@@ -0,0 +1,14 @@
+﻿using CardShop.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CardShop.Data.Configurations
+{
+    public class CardConfig : IEntityTypeConfiguration<Card>
+    {
+        public void Configure(EntityTypeBuilder<Card> builder)
+        {
+
+        }
+    }
+}
