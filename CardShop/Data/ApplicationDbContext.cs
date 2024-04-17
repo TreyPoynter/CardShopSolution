51cd8ce343297d7966c0a7fc00c320e181b19ad2
TreyPoynter <treypoynter@gmail.com>
2024-04-17 11:55:19 -0500

    Add Migration

--- CardShop/Data/ApplicationDbContext.cs
+++ CardShop/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ using System.Reflection.Emit;
 
 namespace CardShop.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
