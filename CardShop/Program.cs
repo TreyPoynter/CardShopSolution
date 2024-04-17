09833c0e0b6cb777c5cc6841c26206d64df36680
TreyPoynter <treypoynter@gmail.com>
2024-04-17 11:11:11 -0500

    Update Program.cs
    formatting

--- CardShop/Program.cs
+++ CardShop/Program.cs
@@ -7,7 +7,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+string connectionName = "DefaultConnection";
+var connectionString = builder.Configuration.GetConnectionString(connectionName) 
+        ?? throw new InvalidOperationException($"Connection string '{connectionName}' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
