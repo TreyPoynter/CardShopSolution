d2f178008d199faf7e168c71c939e972744f57d4
TreyPoynter <treypoynter@gmail.com>
2024-04-17 14:41:28 -0500

    Register / Login Updates

--- CardShop/Areas/Identity/Model/ViewModels/LoginVM.cs
+++ CardShop/Areas/Identity/Model/ViewModels/LoginVM.cs
@@ -2,8 +2,7 @@
 {
     public class LoginVM
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string? Email { get; set; }
         public string? Password { get; set; }
         public bool IsRemembered { get; set; } = false;
     }
---
+++ CardShop/Areas/Identity/Model/ViewModels/RegisterVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CardShop.Areas.Identity.Model.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string? ConfirmPassword { get; set; }
+    }
+}
