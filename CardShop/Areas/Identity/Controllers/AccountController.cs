bd546c97da2a10bbc59699f25c525c13737bacc9
TreyPoynter <treypoynter@gmail.com>
2024-04-17 11:55:41 -0500

    Created Identity Area

---
+++ CardShop/Areas/Identity/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using CardShop.Models.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardShop.Areas.Identity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(UserManager<User> userManager, 
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ CardShop/Areas/Identity/Model/ViewModels/LoginVM.cs
@@ -0,0 +1,10 @@
+﻿namespace CardShop.Areas.Identity.Model.ViewModels
+{
+    public class LoginVM
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Password { get; set; }
+        public bool IsRemembered { get; set; } = false;
+    }
+}
