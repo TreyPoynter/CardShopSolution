4fc244eaea276ea9d90fa68184326dfea35b1a63
TreyPoynter <treypoynter@gmail.com>
2024-04-17 17:31:46 -0500

    Fixed navbar on scroll

---
+++ CardShop/wwwroot/js/navbarTransition.js
@@ -0,0 +1,12 @@
+﻿window.addEventListener("scroll", () => {
+    const navbar = document.getElementById("navbar");
+    const navItems = document.getElementById("nav-items");
+
+    if (window.scrollY >= 33) {
+        navbar.classList.add("fixed-top");
+        navItems.classList.remove("py-3");
+    } else {
+        navbar.classList.remove("fixed-top");
+        navItems.classList.add("py-3");
+    }
+});
