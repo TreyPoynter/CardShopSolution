eb21f5b047be3da0fb82fd884f9f6ac387f5e649
TreyPoynter <treypoynter@gmail.com>
2024-04-18 11:15:22 -0500

    Card route

---
+++ CardShop/Controllers/CardController.cs
@@ -0,0 +1,20 @@
+﻿using CardShop.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardShop.Controllers
+{
+    public class CardController : Controller
+    {
+        [Route("/Cards/{id?}")]
+        public IActionResult Index(string id)
+        {
+            ViewBag.Id = id;
+            CardCategoryVM cardsCategoryVM = new()
+            {
+                Category = id
+            };
+
+            return View(cardsCategoryVM);
+        }
+    }
+}
