bf4210105ea5cd822893096dc4d85e82312e9c12
TreyPoynter <treypoynter@gmail.com>
2024-04-18 11:15:30 -0500

    Create CardCategoryVM.cs

---
+++ CardShop/Models/ViewModels/CardCategoryVM.cs
@@ -0,0 +1,16 @@
+﻿using CardShop.Models.Domain;
+
+namespace CardShop.Models.ViewModels
+{
+    public class CardCategoryVM
+    {
+        public string? Category { get; set; }
+
+        public IEnumerable<Card> Cards { get; set; } = null!;
+
+        public CardCategoryVM()
+        {
+            Cards = new List<Card>();
+        }
+    }
+}
