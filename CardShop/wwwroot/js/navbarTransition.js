d68363dfe67ee5976c5493025a56f2803932e7bc
TreyPoynter <treypoynter@gmail.com>
2024-04-17 21:09:49 -0500

    Updated Navbar

--- CardShop/wwwroot/js/navbarTransition.js
+++ CardShop/wwwroot/js/navbarTransition.js
@@ -1,16 +1,9 @@
 ﻿window.addEventListener("scroll", () => {
-    const nav = document.getElementById("navbar")
-    const upper = document.getElementById("upper-nav");
     const middle = document.getElementById("middle-nav");
-    const below = document.getElementById("bottom-nav");
 
     if (window.scrollY >= 124) {
-        upper.classList.add("invisable");
-        below.classList.add("invisable");
-        nav.classList.add("fixed-top")
+        middle.classList.add("fixed-middle-nav");
     } else {
-        upper.classList.remove("invisable");
-        below.classList.remove("invisable");
-        nav.classList.remove("fixed-top")
+        middle.classList.remove("fixed-middle-nav");
     }
 });
