47714a1e91b120fe2b170b8cd80e2670da28f606
TreyPoynter <treypoynter@gmail.com>
2024-04-19 18:26:07 -0500

    Bag

---
+++ CardShop/Controllers/BagController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CardShop.Controllers
+{
+    public class BagController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
