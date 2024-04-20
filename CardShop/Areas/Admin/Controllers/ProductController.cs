c54c2239da9e1c820112068badd5344b3f7c0454
TreyPoynter <treypoynter@gmail.com>
2024-04-20 10:38:52 -0500

    Basics for Product Controller

---
+++ CardShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardShop.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        [Route("{area}/Products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
