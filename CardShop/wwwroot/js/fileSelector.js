29c143ba3523d5b1ddaa58d7710bdc6eb2c64b57
TreyPoynter <treypoynter@gmail.com>
2024-04-21 13:04:58 -0500

    Responsive Selector

---
+++ CardShop/wwwroot/js/fileSelector.js
@@ -0,0 +1,14 @@
+﻿function updateFileName(input) {
+    const paragraphs = input.parentElement.querySelectorAll('.file-upload-design p');
+
+    if (input.files.length > 0) {
+        const fileName = input.files[0].name;
+        const browseButton = input.parentElement.querySelector('.browse-button');
+        browseButton.textContent = fileName;
+
+        // Show the paragraphs
+        paragraphs.forEach(paragraph => {
+            paragraph.style.display = 'none';
+        });
+    }
+}
