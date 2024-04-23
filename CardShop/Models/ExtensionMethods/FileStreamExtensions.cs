7ca724fc8815663dd48e2f8fc71654f31ba257a6
TreyPoynter <treypoynter@gmail.com>
2024-04-23 05:45:13 -0500

    Create FileStreamExtensions.cs

---
+++ CardShop/Models/ExtensionMethods/FileStreamExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace CardShop.Models.ExtensionMethods
+{
+    public static class FileStreamExtensions
+    {
+        public static void UploadImage(IFormFile file, IWebHostEnvironment hostEnvironment) 
+        {
+            string uploadDir = Path.Combine(hostEnvironment.WebRootPath, "images");
+            string fileName = file.FileName;
+            string filePath = Path.Combine(uploadDir, fileName);
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+        }
+    }
+}
