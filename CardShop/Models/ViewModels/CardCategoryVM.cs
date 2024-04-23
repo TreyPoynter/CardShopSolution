be4ad615a93f3891de7ed3aabbbeda58596e3b6f
TreyPoynter <treypoynter@gmail.com>
2024-04-23 12:26:46 -0500

    UX

--- CardShop/Models/ViewModels/CardCategoryVM.cs
+++ CardShop/Models/ViewModels/CardCategoryVM.cs
@@ -8,6 +8,8 @@ namespace CardShop.Models.ViewModels
 
         public IEnumerable<TradingCard> Cards { get; set; } = null!;
 
+        public bool IsSearching { get; set; } = false;
+
         public CardCategoryVM()
         {
             Cards = new List<TradingCard>();
