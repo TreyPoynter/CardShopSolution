5eb6c645434e83e1efdea45af92ae9eedda6d13d
TreyPoynter <treypoynter@gmail.com>
2024-04-23 17:35:45 -0500

    Update BagController.cs

--- CardShop/Controllers/BagController.cs
+++ CardShop/Controllers/BagController.cs
@@ -4,6 +4,8 @@ namespace CardShop.Controllers
 {
     public class BagController : Controller
     {
+        const string SESSION_KEY = "MyBag";
+
         public IActionResult Index()
         {
             return View();
