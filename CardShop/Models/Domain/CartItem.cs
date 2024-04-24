db443bd51dbaf4a803d4f1408db74f0f084d3f5f
TreyPoynter <treypoynter@gmail.com>
2024-04-23 19:31:16 -0500

    Create CartItem.cs

---
+++ CardShop/Models/Domain/CartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace CardShop.Models.Domain
+{
+    public class CartItem
+    {
+        public string? Id { get; set; }
+        public TradingCard TradingCard { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
