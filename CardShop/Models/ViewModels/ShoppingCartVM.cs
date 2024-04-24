2a76d939397d6a08c317b2024e9d644b30c74406
TreyPoynter <treypoynter@gmail.com>
2024-04-23 19:31:13 -0500

    Create ShoppingCartVM.cs

---
+++ CardShop/Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,17 @@
+﻿using CardShop.Models.Domain;
+
+namespace CardShop.Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public List<CartItem> CartItems { get; set; }
+
+        public decimal TotalPrice { get; set; }
+        public int TotalAmount { get; set; }
+
+        public ShoppingCartVM()
+        {
+            CartItems = new List<CartItem>();
+        }
+    }
+}
