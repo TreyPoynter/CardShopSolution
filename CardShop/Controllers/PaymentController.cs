e274f4c155f1d9bb59ac485b173fcc9fe438f78c
TreyPoynter <treypoynter@gmail.com>
2024-04-23 19:31:18 -0500

    Create PaymentController.cs

---
+++ CardShop/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using CardShop.Models.Domain;
+using CardShop.Models.ExtensionMethods;
+using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
+
+namespace CardShop.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Success()
+        {
+            HttpContext.Session.Set("MyBag", new List<CartItem>());
+            return View();
+        }
+        public IActionResult Cancel()
+        {
+            return View();
+        }
+    }
+}
