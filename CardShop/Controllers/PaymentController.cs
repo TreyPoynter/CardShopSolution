f724c622df66fc54296d3101f1e020e2c61d21c9
TreyPoynter <treypoynter@gmail.com>
2024-04-24 10:04:04 -0500

    Update PaymentController.cs

--- CardShop/Controllers/PaymentController.cs
+++ CardShop/Controllers/PaymentController.cs
@@ -1,10 +1,12 @@
 ﻿using CardShop.Models.Domain;
 using CardShop.Models.ExtensionMethods;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Stripe.Checkout;
 
 namespace CardShop.Controllers
 {
+    [Authorize]
     public class PaymentController : Controller
     {
         public IActionResult Success()
