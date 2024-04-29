c89695b47c3a684dea25cc1fa9499ae5c45db051
TreyPoynter <treypoynter@gmail.com>
2024-04-29 10:45:41 -0500

    Team Manager

--- CardShop/Areas/Admin/Controllers/TeamController.cs
+++ CardShop/Areas/Admin/Controllers/TeamController.cs
@@ -3,10 +3,13 @@ using CardShop.Data;
 using CardShop.Data.Repository;
 using CardShop.Models;
 using CardShop.Models.Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CardShop.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class TeamController : Controller
     {
         private Repository<Team> teamDb { get; set; }
@@ -29,6 +32,47 @@ namespace CardShop.Areas.Admin.Controllers
                 model.Items = model.Items.Where(c => c.Name.ContainsNoCase(searchString) ||
                     c.Name.ContainsNoCase(searchString)).ToList();
 
+            return View(model);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Team? team = teamDb.Get(id);
+
+            if (team == null)
+                return RedirectToAction("Index");
+
+            ManageVM<Team> vm = new()
+            {
+                Action = "Edit",
+                Item = team
+            };
+
+            return View("Manage", vm);
+        }
+
+        public IActionResult Add()
+        {
+            ManageVM<Team> vm = new()
+            {
+                Action = "Add",
+                Item = new Team()
+            };
+
+            return View("Manage", vm);
+        }
+
+        public IActionResult Manage(ManageVM<Team> model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(model.Item.TeamId == 0)
+                    teamDb.Add(model.Item);
+                else
+                    teamDb.Update(model.Item);
+                teamDb.Save();
+                return RedirectToAction("Index");
+            }
             return View(model);
         }
     }
---
+++ CardShop/Areas/Admin/Models/ViewModels/ManageVM.cs
@@ -0,0 +1,8 @@
+﻿namespace CardShop.Areas.Admin.Models.ViewModels
+{
+    public class ManageVM<T> where T : class
+    {
+        public string Action { get; set; } = String.Empty;
+        public T? Item { get; set; }
+    }
+}
