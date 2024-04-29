d9df1d24f9d782a6bdb191ee7774b11b1a4ed7fc
TreyPoynter <treypoynter@gmail.com>
2024-04-29 09:13:36 -0500

    Team View

--- CardShop/Areas/Admin/Controllers/TeamController.cs
+++ CardShop/Areas/Admin/Controllers/TeamController.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CardShop.Areas.Admin.Models.ViewModels;
+using CardShop.Data;
+using CardShop.Data.Repository;
+using CardShop.Models;
+using CardShop.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CardShop.Areas.Admin.Controllers
 {
     public class TeamController : Controller
     {
-        [Route("{area}/Teams")]
-        public IActionResult Index()
+        private Repository<Team> teamDb { get; set; }
+
+        public TeamController(ApplicationDbContext ctx)
         {
-            return View();
+            teamDb = new Repository<Team>(ctx);
+        }
+
+        [Route("{area}/Teams")]
+        public IActionResult Index(string searchString = "")
+        {
+            SearchVM<Team> model = new SearchVM<Team>()
+            {
+                Search = searchString,
+                Items = teamDb.List(new QueryOptions<Team>())
+            };
+
+            if (searchString != String.Empty)
+                model.Items = model.Items.Where(c => c.Name.ContainsNoCase(searchString) ||
+                    c.Name.ContainsNoCase(searchString)).ToList();
+
+            return View(model);
         }
     }
 }
