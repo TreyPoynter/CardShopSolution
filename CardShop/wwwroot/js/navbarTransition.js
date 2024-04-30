8bb9a3b6c657e3c089aa33c0ffced4cbd8af0555
TreyPoynter <treypoynter@gmail.com>
2024-04-30 11:48:39 -0500

    Update navbarTransition.js
    fixed errors on scroll

--- CardShop/wwwroot/js/navbarTransition.js
+++ CardShop/wwwroot/js/navbarTransition.js
@@ -1,9 +1,11 @@
 ﻿window.addEventListener("scroll", () => {
     const middle = document.getElementById("fixed-middle-nav");
 
-    if (window.scrollY >= 124) {
-        middle.classList.remove("invisible");
-    } else {
-        middle.classList.add("invisible");
+    if (middle != null) {
+        if (window.scrollY >= 124) {
+            middle.classList.remove("invisible");
+        } else {
+            middle.classList.add("invisible");
+        }
     }
 });
